8f3198a79d776c307f8408cf0b8c98888a1a90b6
gooddle <vptl6559@gmail.com>
2024-05-22 11:10:24 +0900

    :coffin: 전체 댓글 기능 삭제

--- src/main/kotlin/com/todolist/todo1/domain/comment/service/CommentService.kt
+++ src/main/kotlin/com/todolist/todo1/domain/comment/service/CommentService.kt
@@ -6,7 +6,6 @@ import com.todolist.todo1.domain.comment.dto.DeleteCommentRequest
 import com.todolist.todo1.domain.comment.dto.UpdateCommentRequest
 
 interface CommentService {
-    fun getComments(todoId: Long) : List<CommentResponse>
     fun getCreateComment(todoId: Long,request: CreateCommentRequest) : CommentResponse
     fun getUpdateComment(todoId: Long, commentId : Long,request: UpdateCommentRequest  ) : CommentResponse
     fun getDeleteComment(todoId: Long, commentId : Long,request: DeleteCommentRequest)
