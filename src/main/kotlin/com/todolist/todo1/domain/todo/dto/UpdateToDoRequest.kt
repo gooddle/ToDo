c57fcc0f9fd99c631fae4d9b18fc8cfd6ac27d03
gooddle <vptl6559@gmail.com>
2024-05-22 12:35:21 +0900

    :hammer:  글자수 제한 기능 추가

--- src/main/kotlin/com/todolist/todo1/domain/todo/dto/UpdateToDoRequest.kt
+++ src/main/kotlin/com/todolist/todo1/domain/todo/dto/UpdateToDoRequest.kt
@@ -1,9 +1,15 @@
 package com.todolist.todo1.domain.todo.dto
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 
 data class UpdateToDoRequest(
+    @field:NotBlank
+    @field:Size(min = 1, max = 200)
     val title: String,
+    @field:NotBlank
+    @field:Size(min = 1, max = 1000)
     val description: String,
     var name : String,
     var status : Boolean
