27eb3e1899d71912c900a669d4780c6bcf9c938c
gooddle <vptl6559@gmail.com>
2024-05-22 11:12:12 +0900

    :fire: 작성자 필터링 기능 및 목록 정렬 기능 추가

--- src/main/kotlin/com/todolist/todo1/domain/todo/repository/ToDoRepository.kt
+++ src/main/kotlin/com/todolist/todo1/domain/todo/repository/ToDoRepository.kt
@@ -2,10 +2,15 @@ package com.todolist.todo1.domain.todo.repository
 
 
 import com.todolist.todo1.domain.todo.model.ToDo
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+
+
 import org.springframework.data.jpa.repository.JpaRepository
 
 
 
-interface ToDoRepository: JpaRepository<ToDo, Long> {
 
+interface ToDoRepository: JpaRepository<ToDo, Long> {
+ fun findByNameContainingIgnoreCase(name:String?,pageable: Pageable): Page<ToDo>
 }
