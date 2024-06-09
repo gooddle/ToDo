eb53b421de26736d0a66f4b15cab4ea1555cefaf
gooddle <vptl6559@gmail.com>
2024-06-09 21:23:47 +0900

    :zap: 에플리케이션 어노테이션 추가

--- src/main/kotlin/com/todolist/todo1/ToDo1Application.kt
+++ src/main/kotlin/com/todolist/todo1/ToDo1Application.kt
@@ -2,7 +2,9 @@ package com.todolist.todo1
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.EnableAspectJAutoProxy
 
+@EnableAspectJAutoProxy
 @SpringBootApplication
 class ToDo1Application
 
